137168690deb761338e8ec76f06d1e08c113df44
Suraj <suraj2612@gmail.com>
2020-03-15 16:44:27 -0400

    Adds user table

---
+++ backend/db/migrate/20200315203910_add_users.rb
@@ -0,0 +1,8 @@
+class AddUsers < ActiveRecord::Migration[5.2]
+  def change
+    create_table :user, id: :uuid do |t|
+      t.string :password_digest, null: :false
+      t.timestamps null: false
+    end
+  end
+end
